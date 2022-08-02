754dba5a024c0fa117d0e79eee4da7252a107a00
teilon <teiloncw@gmail.com>
2022-08-02 23:52:58 +0600

    add timer

--- web/app/bot.py
+++ web/app/bot.py
@@ -2,7 +2,11 @@ import logging
 import os
 
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, updater
+from telegram.ext import (
+    Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+)
+
+from timer_bot import set_timer, unset
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -20,6 +24,8 @@ def start(update: Update, context: CallbackContext) -> None:
         fr'Hi {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True),
     )
+    """Sends explanation on how to use the bot."""
+    update.message.reply_text("Hi! Use /set <seconds> to set a timer")
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -38,6 +44,10 @@ def start_bot() -> None:
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("set", set_timer))
+    dispatcher.add_handler(CommandHandler("unset", unset))
 
     updater.start_polling()
     # updater.idle()
